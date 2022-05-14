6fd272b47e2795ff5f982095acc390ee8f6fad98
miguel javela <mjavela@compuamerica.com.ve>
2022-05-13 21:28:30 -0500

    clase 8 - this

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,9 @@ import MediaPlayer from './mediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins:[new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins:[
+  //new AutoPlay()
+] });
 
 const buttonPlay = document.getElementById('play');
 buttonPlay.onclick = () => player.togglePlay();

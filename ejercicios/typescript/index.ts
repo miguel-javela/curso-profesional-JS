f5f59d6b883153f782cf7a17c127772dee107379
miguel javela <mjavela@compuamerica.com.ve>
2022-05-16 23:16:01 -0500

    clase 25 - tipos basicos TS

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -5,3 +5,37 @@ function add(a:number, b:number){
 }
 
 const sum = add(2,3)
+
+//boolean
+let muted:boolean = true;
+
+//numeros
+let numerador: number = 42
+let denominador: number = 6
+
+//string
+
+//arreglos
+let people: string[] = [];//arreglo de strings
+people = ["isa", "raul"];
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("ricardo");
+peopleAndNumbers.push(90)
+
+//enum
+enum Color{
+    rojo = "rojo",
+    verde = "verde",
+    azul = "azul"
+}
+
+let colorFavorito : Color = Color.verde;
+console.log(`colo fav ${colorFavorito}`)
+
+//any
+let comodin: any= "joker";
+comodin = {type: 'wildcard'}
+
+//object
+let someObject: Object = {type: 'wildcard'}

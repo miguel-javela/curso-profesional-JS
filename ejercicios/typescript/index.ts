d41961c5cd12833518826d5edd005dbee2a66bf5
miguel javela <mjavela@compuamerica.com.ve>
2022-05-17 22:42:10 -0500

    clase 27 - interfaces

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -57,3 +57,32 @@ comodin = {type: 'wildcard'}
 
 //object
 let someObject: Object = {type: 'wildcard'}
+
+enum Color{
+    Rojo= "Rojo",
+    Verde = "Verde"
+}
+
+interface Rectangulo{
+    ancho: number;
+    alto: number;
+    color?:Color
+}
+
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    color: Color.Rojo
+}
+
+function area(r:Rectangulo){
+    return r.alto*r.ancho
+}
+const areaRect = area(rect);
+console.log(areaRect);
+
+console.log(rect.toString());
+rect.toString = function (){
+    return this.color ? `Un rectangulo ${this.color}`: `un Rectangulo`;
+}
+console.log(rect.toString());
